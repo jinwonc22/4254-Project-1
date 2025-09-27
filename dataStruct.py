2ad0770bf3e02eb6c5b47d7f3387d951068f63d6
ConnorMcCue23 <connormccue23@vt.edu>
2025-09-27 10:53:36 -0400

    Base working code

--- dataStruct.py
+++ dataStruct.py
@@ -1,10 +1,27 @@
 import json
 
 class Shape:
-    
-    def __init__(self, pos, color, type):
+    def __init__(self, pos, color, shape_type):
         self.pos = pos
         self.color = color
-        self.type = type
+        self.shape_type = shape_type
 
-        
+    def to_dict(self):
+        return {
+            "position": self.pos,
+            "color": self.color,
+            "shape_type": self.shape_type
+        }
+
+shapes = []
+
+s = Shape((10, 20), "red", "circle")
+
+shapes.append(s)
+
+shapes_json = json.dumps([shape.to_dict() for shape in shapes], indent=2)
+
+print(shapes_json)
+
+with open("shapes.json", "w") as f:
+    f.write(shapes_json)
